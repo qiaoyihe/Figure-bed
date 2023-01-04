d088f908802e75d11203662df924754523421ca4
乔一禾 <728754480@qq.com>
2023-01-04 20:43:02 +0800

    Add files via upload

---
+++ hexo-mood.js
@@ -0,0 +1,35 @@
+<script>
+        $(function () {
+            function isEmpty(obj) {
+                if (typeof obj == "undefined" || obj == null || obj == "") {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        })
+        var items = document.querySelectorAll(".timeline li");
+
+        function isElementInViewport(el) {
+            var rect = el.getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+        }
+        function callbackFunc() {
+            for (var i = 0; i < items.length; i++) {
+                if (isElementInViewport(items[i])) {
+                    if (!items[i].classList.contains("in-view")) {
+                        items[i].classList.add("in-view");
+                    }
+                } else if (items[i].classList.contains("in-view")) {
+                    items[i].classList.remove("in-view");
+                }
+            }
+        }
+        window.addEventListener("load", callbackFunc);
+        window.addEventListener("scroll", callbackFunc);
+</script>
